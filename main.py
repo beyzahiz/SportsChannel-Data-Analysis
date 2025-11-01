1d033406250256989a418f255dee01e916de9f5d
Beyza Hız <beyzahiz.03@gmail.com>
2025-11-01 16:48:23 +0300

    new columns

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import pandas as pd
 #csv'nin olduğu dosya yolunu yazmak gerekiyor
 veri = pd.read_csv("data/yt_sports_channels_stats.csv")
 
-first =veri.head()
-print(first)
-
-bilgi = veri.info()
-print(bilgi)
-
+#veri.head()
+#veri.info()
 nan = veri.isna().sum()
-print(nan)
+#print(nan)
 
 #start_date sütunundaki veriyi datetime tipine dönüştürdüm dönüşemeyenler NaT oldu
 veri["start_date"] = pd.to_datetime(veri["start_date"], errors="coerce")
 veri.info()
 
-veri = veri.dropna(subset=["start_date"])
-veri.shape
-veri.info()
-veri.head()
+#start_date'deki nan verileri sildim
+veri = veri.dropna(subset=["start_date"]) 
+# veri.shape
+# veri.info()
+# veri.head()
+
+veri["start_date"] = veri["start_date"].dt.tz_convert(None) #saat dilimi bilgisini kaldırır
+today = pd.Timestamp.today()
+veri["channel_age"] = (today-veri["start_date"]).dt.days/365
+veri["views_per_video"] = veri["view_count"] / veri["video_count"]
+veri["subs_per_view"] = veri["subscriber_count"] / veri["view_count"]
+print(veri.head())
+print(veri.info())
 
